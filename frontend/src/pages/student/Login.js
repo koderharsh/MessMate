f0b2cfe011573bfa8f85cfcb2ac87e284054c6a3
Aditya Paul <aditya.paul18@gmail.com>
2021-03-09 10:15:53 +0530

    signin page start

--- frontend/src/pages/student/Login.js
+++ frontend/src/pages/student/Login.js
@@ -1,7 +1,13 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import {login,authenticate} from "../../util/studentApi";
 import {useHistory} from 'react-router-dom'
-
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import IconButton from '@material-ui/core/IconButton';
+import redImage from '../student/Dashboard/raspberries.jpg'
+import greenImage from '../student/Dashboard/grapes.jpg'
+import blueImage from '../student/Dashboard/blueberry2.jpg'
+import orangeImage from '../student/Dashboard/pasta.jpg'
+import yellowImage from '../student/Dashboard/mango1.jpg' 
 const Login=()=>{
 
   const  history= useHistory();
@@ -43,21 +49,71 @@ const Login=()=>{
   }
 })
 }
+
+useEffect(() => {
+  applyAccent()
+  console.log("yaya dfsdfjkjs");
+}, []);
+
 //ugly demo
   return(
     <div>
-      <div id="navbar-wrapper">
-          <span id="navleft"><i className="lni lni-dinner"></i> MESSMATE</span>
-          <span id="navright">
-          </span>
+      <div id='dashboard-wrapper'>
+      <div className="dashboard__title__wrapper">
+        <div>Messmate</div>
+        <div>
+          STUDENT • 
+          <div>
+          <IconButton aria-label="delete" onClick={()=>{
+              localStorage.removeItem("student");
+              history.push("/login/student");
+            }}>
+              <PowerSettingsNewIcon
+              className="logoutButton"
+          />
+          </IconButton>
+          </div>
+        </div>
       </div>
-    <form>
-    <input name="email" type="email" value={email} onChange={handleChange("email")}/>
-    <input name="password" type="password" value={password} onChange={handleChange("password")}/>
-    <button type="submit" onClick={handleSubmit}>Login</button>
-    </form>
+
+      <div id='cardgrid'>
+        <div className='cardgrid__card' id='card1'>
+          <form>
+              <input name="email" type="email" value={email} onChange={handleChange("email")}/>
+              <input name="password" type="password" value={password} onChange={handleChange("password")}/>
+              <button type="submit" onClick={handleSubmit}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div id='messImg-wrapper'>
+        <div></div>
+      </div>
+   
     </div>
   )
+}
+
+
+function applyAccent() {
+  let accentNum = localStorage.getItem('accentNum') || 0
+  let accentCodes = ['235, 50, 50', '0, 200, 33', '232, 232, 0', '0, 96, 206', '255, 61, 12']
+  let backgroundImages = [redImage, greenImage, yellowImage, blueImage, orangeImage]
+  document.querySelector(':root').style.setProperty('--accent', accentCodes[accentNum])
+  document.getElementById('messImg-wrapper').style.backgroundImage = `url(${backgroundImages[accentNum]})`
+  document.getElementById('messImg-wrapper').style.backgroundSize = "cover"
+
+  if(window.innerWidth <= 1300) {
+    document.body.style.backgroundImage = `url(${backgroundImages[accentNum]})`
+    document.body.style.backgroundSize = "cover"
+  }
+  document.body.onresize = () => {
+    if(window.innerWidth <= 1300) {
+      document.body.style.backgroundImage = `url(${backgroundImages[accentNum]})`
+      document.body.style.backgroundSize = "cover"
+    }
+    else document.body.style.backgroundImage = "none"
+  }
 }
 
 export default Login;
