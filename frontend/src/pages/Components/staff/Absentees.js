33022a08d86772c7f5d9d8a7abafbb43ec4d6f28
heli576 <helivakharia@gmail.com>
2021-03-14 20:45:49 +0530

    Modified absentees component

--- frontend/src/pages/Components/staff/Absentees.js
+++ frontend/src/pages/Components/staff/Absentees.js
@@ -3,6 +3,7 @@ import { isAuthenticated, getAbsentees } from "../../../util/staffApi";
 import "../student/studentComponentStyles.css"
 import IconButton from '@material-ui/core/IconButton';
 import { PieChart } from 'react-minimal-pie-chart';
+import Typography from '@material-ui/core/Typography';
 
 const Absentees=()=>{
   const token=isAuthenticated()&&isAuthenticated().stafftoken;
@@ -11,29 +12,36 @@ const Absentees=()=>{
   const [total,setTotal]=useState("");
   const [meal,setMeal]=useState("");
   const [val,setVal]=useState("");
+  const [error,setError]=useState("");
   const lineWidth = 60;
 
   useEffect(()=>{
-    getAbsentees(token)
-    .then((data,error)=>{
-      if(error)
-      console.log(error)
-      else{
-        console.log(data)
-        setTotal(data.total);
+    const hour=new Date().getHours();
+    if(hour<10){
+    setMeal("Breakfast");
+  }
+    else if(hour>=10&&hour<=15){
+        setMeal("Lunch");
+    }
 
-        const hour=new Date().getHours();
+    else {
+      setMeal("Dinner");
+    }
+    getAbsentees(token)
+    .then((data)=>{
+      if(data.error)
+      setError(data.error)
+      else{
+        //console.log(data)
+        setTotal(data.total);
         if(hour<10){
-        setMeal("Breakfast");
         setVal(data.breakfast.count);
       }
         else if(hour>=10&&hour<=15){
-            setMeal("Lunch");
             setVal(data.lunch.count);
         }
 
         else {
-          setMeal("Dinner");
           setVal(data.dinner.count);
         }
 
@@ -43,29 +51,39 @@ const Absentees=()=>{
   return(
     <div>
         <h3 className="student-card-heading">Absentees for {meal}</h3>
-        <PieChart
-data={[
- { title: 'Absentees', value:val, color: 'rgba( 32, 31, 31, 0.50 )' },
- { title: 'Attendees', value: total-val, color: 'rgba(var(--accent), 0.9)' },
-]}
-style={{
-       fontFamily:
-         '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
-       fontSize: '6px',
-     }}
-     radius={35}
-     lineWidth={60}
-     segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
-     animate
-     label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
-     labelPosition={100 - lineWidth / 2}
-     labelStyle={{
-       fill: '#fff',
-       opacity: 0.75,
-       pointerEvents: 'none',
-     }}
+        {error?(  <div className="absence__tiptext">
+               No records found!!
+          </div>):(<div>
+            <div className="absence__tiptext">
+                  Number of absentees: {val}
+              </div>
+          <PieChart
+  data={[
+   { title: 'Absentees', value:val, color: 'rgba( 32, 31, 31, 0.50 )' },
+   { title: 'Attendees', value: total-val, color: 'rgba(var(--accent), 0.9)' },
+  ]}
+  style={{
+    marginTop:-50,
+         fontFamily:
+           '"Nunito Sans", -apple-system, Helvetica, Arial, sans-serif',
+         fontSize: '6px',
+       }}
+       radius={30}
+       lineWidth={60}
+       segmentsStyle={{ transition: 'stroke .3s', cursor: 'pointer' }}
+       animate
+       label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
+       labelPosition={100 - lineWidth / 2}
+       labelStyle={{
+         fill: '#fff',
+         opacity: 0.75,
+         pointerEvents: 'none',
+       }}
 
-/>
+  />
+</div>
+        )}
+
     </div>
   )
 }
